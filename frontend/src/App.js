7b7ee93b941de6837c3409be64e399ca1f0f6725
Claret <utm23090735@utma.edu.mx>
2024-11-10 15:04:56 -0600

    frontend hecho

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -27,20 +27,41 @@ function App() {
         <Card.Body>
           <Card.Title>Formulario para Evaluación de Tutores</Card.Title>
 
+ {/* Información alumno */}
+          <Card className='mt-3'>
+            <Card.Body>
+              <h5>Información Alumno</h5>
+              <Form.Group className='mb-3'>
+                <Form.Label>Correo institucional</Form.Label>
+                <Form.Control
+                  type="email"
+                  name="email"
+                  placeholder="Ingresa tu correo institucional"
+                  value={formulario.email || ""}
+                  onChange={onChange}
+                />
+              </Form.Group>
+              <Form.Group className='mb-3'>
+                <Form.Label>Nombre completo</Form.Label>
+                <Form.Control onChange={onChange} placeholder='Ingresa el nombre del estudiante' name='StudentName' />
+              </Form.Group>
+              <Form.Group className='mb-3'>
+                <Form.Label>¿A qué grupo perteneces?</Form.Label>
+                <Form.Control onChange={onChange} placeholder='Ingresa tu grupo' name='grupo' />
+              </Form.Group>
+            </Card.Body>
+          </Card>
+
           {/* Información General */}
           <Card className='mt-3'>
             <Card.Body>
               <h5>Información General</h5>
               <Form.Group className='mb-3'>
-                <Form.Label>Nombre del Tutor</Form.Label>
+                <Form.Label>¿Quién es tu tutor?</Form.Label>
                 <Form.Control onChange={onChange} placeholder='Ingresa el nombre del tutor' name='TutorName' />
               </Form.Group>
               <Form.Group className='mb-3'>
-                <Form.Label>Nombre del Estudiante</Form.Label>
-                <Form.Control onChange={onChange} placeholder='Ingresa el nombre del estudiante' name='StudentName' />
-              </Form.Group>
-              <Form.Group className='mb-3'>
-                <Form.Label>Asignatura</Form.Label>
+                <Form.Label>¿Qué asignatura estás tomando?</Form.Label>
                 <Form.Control onChange={onChange} placeholder='Ingresa la asignatura' name='Subject' />
               </Form.Group>
             </Card.Body>
@@ -52,7 +73,7 @@ function App() {
               <h5>Preparación y Conocimiento</h5>
               <Form.Group className='mb-3'>
                 <Form.Label>¿El tutor demuestra un buen conocimiento de la materia?</Form.Label>
-                <Form.Control as='textarea' rows={2} onChange={onChange} name='Knowledge' />
+                <Form.Control as='textarea' rows={2} onChange={onChange} name='conocimiento' />
               </Form.Group>
               <Form.Group className='mb-3'>
                 <Form.Label>¿El tutor está bien preparado para las sesiones?</Form.Label>
@@ -67,7 +88,7 @@ function App() {
               <h5>Métodos de Enseñanza</h5>
               <Form.Group className='mb-3'>
                 <Form.Label>¿El tutor explica los conceptos claramente?</Form.Label>
-                <Form.Control as='textarea' rows={2} onChange={onChange} name='Clarity' />
+                <Form.Control as='textarea' rows={2} onChange={onChange} name='Conceptos' />
               </Form.Group>
               <Form.Group className='mb-3'>
                 <Form.Label>¿El tutor utiliza ejemplos prácticos para facilitar el aprendizaje?</Form.Label>
@@ -75,7 +96,7 @@ function App() {
               </Form.Group>
               <Form.Group className='mb-3'>
                 <Form.Label>¿El tutor adapta sus métodos de enseñanza según las necesidades del estudiante?</Form.Label>
-                <Form.Control as='textarea' rows={2} onChange={onChange} name='Adaptability' />
+                <Form.Control as='textarea' rows={2} onChange={onChange} name='enseñanza' />
               </Form.Group>
             </Card.Body>
           </Card>
@@ -90,11 +111,11 @@ function App() {
               </Form.Group>
               <Form.Group className='mb-3'>
                 <Form.Label>¿El tutor está disponible para responder preguntas fuera de las horas de clase?</Form.Label>
-                <Form.Control as='textarea' rows={2} onChange={onChange} name='Availability' />
+                <Form.Control as='textarea' rows={2} onChange={onChange} name='disponibilidad' />
               </Form.Group>
               <Form.Group className='mb-3'>
                 <Form.Label>¿El tutor es paciente y comprensivo?</Form.Label>
-                <Form.Control as='textarea' rows={2} onChange={onChange} name='Patience' />
+                <Form.Control as='textarea' rows={2} onChange={onChange} name='comprension' />
               </Form.Group>
             </Card.Body>
           </Card>
@@ -105,7 +126,7 @@ function App() {
               <h5>Compromiso y Puntualidad</h5>
               <Form.Group className='mb-3'>
                 <Form.Label>¿El tutor es puntual y respetuoso con el tiempo?</Form.Label>
-                <Form.Control as='textarea' rows={2} onChange={onChange} name='Punctuality' />
+                <Form.Control as='textarea' rows={2} onChange={onChange} name='puntualidad' />
               </Form.Group>
               <Form.Group className='mb-3'>
                 <Form.Label>¿El tutor demuestra interés genuino en el progreso del estudiante?</Form.Label>
@@ -120,7 +141,7 @@ function App() {
               <h5>Ambiente de Aprendizaje</h5>
               <Form.Group className='mb-3'>
                 <Form.Label>¿El tutor crea un ambiente de aprendizaje positivo y motivador?</Form.Label>
-                <Form.Control as='textarea' rows={2} onChange={onChange} name='Environment' />
+                <Form.Control as='textarea' rows={2} onChange={onChange} name='aprendizaje' />
               </Form.Group>
               <Form.Group className='mb-3'>
                 <Form.Label>¿El tutor motiva al estudiante a participar activamente en las sesiones?</Form.Label>
@@ -135,11 +156,11 @@ function App() {
               <h5>Resultado del Aprendizaje</h5>
               <Form.Group className='mb-3'>
                 <Form.Label>¿El tutor ayuda a mejorar las habilidades y el rendimiento académico del estudiante?</Form.Label>
-                <Form.Control as='textarea' rows={2} onChange={onChange} name='SkillImprovement' />
+                <Form.Control as='textarea' rows={2} onChange={onChange} name='rendimiento' />
               </Form.Group>
               <Form.Group className='mb-3'>
                 <Form.Label>¿El tutor proporciona retroalimentación constructiva y útil?</Form.Label>
-                <Form.Control as='textarea' rows={2} onChange={onChange} name='Feedback' />
+                <Form.Control as='textarea' rows={2} onChange={onChange} name='retroalimentacion' />
               </Form.Group>
             </Card.Body>
           </Card>
@@ -154,7 +175,7 @@ function App() {
               </Form.Group>
               <Form.Group className='mb-3'>
                 <Form.Label>¿Hay algo que el tutor pueda mejorar?</Form.Label>
-                <Form.Control as='textarea' rows={2} onChange={onChange} name='Improvement' />
+                <Form.Control as='textarea' rows={2} onChange={onChange} name='mejorar' />
               </Form.Group>
               <Form.Group className='mb-3'>
                 <Form.Label>¿Recomendarías al tutor a otros estudiantes?</Form.Label>
