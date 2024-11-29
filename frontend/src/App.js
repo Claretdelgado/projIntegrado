c7893ca84807f5b5432e20fa5614a4f923657e1c
Claret <utm23090735@utma.edu.mx>
2024-11-28 21:25:24 -0600

    modificacion usermodels

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,6 +6,22 @@ function App() {
   const [formulario, setFormulario] = useState({});
   const [isEnabled, setIsEnabled] = useState(true);
 
+
+  const questionnarie = {
+    preguntas: [
+      "¿El tutor demuestra un buen conocimiento de la materia?",
+      "¿El tutor está bien preparado para las sesiones?",
+      "¿El tutor explica los conceptos claramente?",
+      "¿El tutor utiliza ejemplos prácticos para facilitar el aprendizaje?",
+      "¿El tutor adapta sus métodos de enseñanza según las necesidades del estudiante?",
+      "¿El tutor fomenta una buena comunicación?",
+      "¿El tutor es puntual y respetuoso con el tiempo?",
+      "¿El tutor demuestra interés genuino en el progreso del estudiante?"
+    ],
+    opciones: ["Siempre", "Casi siempre", "A veces", "Nunca"]
+  };
+  
+
   const onChange = (e) => {
     e.preventDefault();
     const obj = { ...formulario };
@@ -21,177 +37,60 @@ function App() {
     }
   };
 
-  return (
-    <Container>
-      <Card className='mt-3'>
-        <Card.Body>
-          <Card.Title>Formulario para Evaluación de Tutores</Card.Title>
+  const onSumbit = async () => {
+    //validar que las preguntas fueron contestadas
+    const questionsUnanswered = [];
+    questionnarie.preguntas.map((_, i) => {
+      if (!answers[`pregunta_${i}`]) {
+        questionsUnanswered.push(i + 1)
+      }
+  })
+  if (questionsUnanswered.length > 0) {
+    Swal.fire(
+      "ops, parece que faltan preguntas por contestar", questionsUnanswered.join(`, `), "error");
+      return;
+  
+  }
+  Swal.fire("enviando respuestas")
+  Swal.showLoading()
+  try {
+    await axios.post("http:localhost:4000/save-answers", answers);
+    Swal.fire("respuestas almacenadas con exito","","success")
+  } catch (error) {
+    Swal.fire("ocurrio un error al guardar las respuestas", error)
+  }
+}
 
- {/* Información alumno */}
-          <Card className='mt-3'>
-            <Card.Body>
-              <h5>Información Alumno</h5>
-              <Form.Group className='mb-3'>
-                <Form.Label>Correo institucional</Form.Label>
-                <Form.Control
-                  type="email"
-                  name="email"
-                  placeholder="Ingresa tu correo institucional"
-                  value={formulario.email || ""}
+return (
+  <Container className="mt-3">
+    <Card>
+      <Card.Body>
+        <Card.Title>Formulario de evaluación docentes</Card.Title>
+        <Form onSubmit={onSubmit}>
+          {questionnarie.preguntas.map((pregunta, i) => (
+            <Form.Group key={`q-${i}`} className="mb-3">
+              <Form.Label>{`${i + 1}. ${pregunta}`}</Form.Label>
+              {questionnarie.opciones.map((opcion, io) => (
+                <Form.Check
+                  key={`${i}-${io}`}
+                  type="radio"
+                  id={`q${i}-option${io}`}
+                  name={`pregunta_${i}`}
+                  value={opcion}
+                  label={opcion}
                   onChange={onChange}
                 />
-              </Form.Group>
-              <Form.Group className='mb-3'>
-                <Form.Label>Nombre completo</Form.Label>
-                <Form.Control onChange={onChange} placeholder='Ingresa el nombre del estudiante' name='StudentName' />
-              </Form.Group>
-              <Form.Group className='mb-3'>
-                <Form.Label>¿A qué grupo perteneces?</Form.Label>
-                <Form.Control onChange={onChange} placeholder='Ingresa tu grupo' name='grupo' />
-              </Form.Group>
-            </Card.Body>
-          </Card>
-
-          {/* Información General */}
-          <Card className='mt-3'>
-            <Card.Body>
-              <h5>Información General</h5>
-              <Form.Group className='mb-3'>
-                <Form.Label>¿Quién es tu tutor?</Form.Label>
-                <Form.Control onChange={onChange} placeholder='Ingresa el nombre del tutor' name='TutorName' />
-              </Form.Group>
-              <Form.Group className='mb-3'>
-                <Form.Label>¿Qué asignatura estás tomando?</Form.Label>
-                <Form.Control onChange={onChange} placeholder='Ingresa la asignatura' name='Subject' />
-              </Form.Group>
-            </Card.Body>
-          </Card>
-
-          {/* Preparación y Conocimiento */}
-          <Card className='mt-3'>
-            <Card.Body>
-              <h5>Preparación y Conocimiento</h5>
-              <Form.Group className='mb-3'>
-                <Form.Label>¿El tutor demuestra un buen conocimiento de la materia?</Form.Label>
-                <Form.Control as='textarea' rows={2} onChange={onChange} name='conocimiento' />
-              </Form.Group>
-              <Form.Group className='mb-3'>
-                <Form.Label>¿El tutor está bien preparado para las sesiones?</Form.Label>
-                <Form.Control as='textarea' rows={2} onChange={onChange} name='Preparation' />
-              </Form.Group>
-            </Card.Body>
-          </Card>
-
-          {/* Métodos de Enseñanza */}
-          <Card className='mt-3'>
-            <Card.Body>
-              <h5>Métodos de Enseñanza</h5>
-              <Form.Group className='mb-3'>
-                <Form.Label>¿El tutor explica los conceptos claramente?</Form.Label>
-                <Form.Control as='textarea' rows={2} onChange={onChange} name='Conceptos' />
-              </Form.Group>
-              <Form.Group className='mb-3'>
-                <Form.Label>¿El tutor utiliza ejemplos prácticos para facilitar el aprendizaje?</Form.Label>
-                <Form.Control as='textarea' rows={2} onChange={onChange} name='Examples' />
-              </Form.Group>
-              <Form.Group className='mb-3'>
-                <Form.Label>¿El tutor adapta sus métodos de enseñanza según las necesidades del estudiante?</Form.Label>
-                <Form.Control as='textarea' rows={2} onChange={onChange} name='enseñanza' />
-              </Form.Group>
-            </Card.Body>
-          </Card>
-
-          {/* Interacción y Comunicación */}
-          <Card className='mt-3'>
-            <Card.Body>
-              <h5>Interacción y Comunicación</h5>
-              <Form.Group className='mb-3'>
-                <Form.Label>¿El tutor fomenta una buena comunicación?</Form.Label>
-                <Form.Control as='textarea' rows={2} onChange={onChange} name='Communication' />
-              </Form.Group>
-              <Form.Group className='mb-3'>
-                <Form.Label>¿El tutor está disponible para responder preguntas fuera de las horas de clase?</Form.Label>
-                <Form.Control as='textarea' rows={2} onChange={onChange} name='disponibilidad' />
-              </Form.Group>
-              <Form.Group className='mb-3'>
-                <Form.Label>¿El tutor es paciente y comprensivo?</Form.Label>
-                <Form.Control as='textarea' rows={2} onChange={onChange} name='comprension' />
-              </Form.Group>
-            </Card.Body>
-          </Card>
-
-          {/* Compromiso y Puntualidad */}
-          <Card className='mt-3'>
-            <Card.Body>
-              <h5>Compromiso y Puntualidad</h5>
-              <Form.Group className='mb-3'>
-                <Form.Label>¿El tutor es puntual y respetuoso con el tiempo?</Form.Label>
-                <Form.Control as='textarea' rows={2} onChange={onChange} name='puntualidad' />
-              </Form.Group>
-              <Form.Group className='mb-3'>
-                <Form.Label>¿El tutor demuestra interés genuino en el progreso del estudiante?</Form.Label>
-                <Form.Control as='textarea' rows={2} onChange={onChange} name='Interest' />
-              </Form.Group>
-            </Card.Body>
-          </Card>
-
-          {/* Ambiente de Aprendizaje */}
-          <Card className='mt-3'>
-            <Card.Body>
-              <h5>Ambiente de Aprendizaje</h5>
-              <Form.Group className='mb-3'>
-                <Form.Label>¿El tutor crea un ambiente de aprendizaje positivo y motivador?</Form.Label>
-                <Form.Control as='textarea' rows={2} onChange={onChange} name='aprendizaje' />
-              </Form.Group>
-              <Form.Group className='mb-3'>
-                <Form.Label>¿El tutor motiva al estudiante a participar activamente en las sesiones?</Form.Label>
-                <Form.Control as='textarea' rows={2} onChange={onChange} name='Motivation' />
-              </Form.Group>
-            </Card.Body>
-          </Card>
-
-          {/* Resultado del Aprendizaje */}
-          <Card className='mt-3'>
-            <Card.Body>
-              <h5>Resultado del Aprendizaje</h5>
-              <Form.Group className='mb-3'>
-                <Form.Label>¿El tutor ayuda a mejorar las habilidades y el rendimiento académico del estudiante?</Form.Label>
-                <Form.Control as='textarea' rows={2} onChange={onChange} name='rendimiento' />
-              </Form.Group>
-              <Form.Group className='mb-3'>
-                <Form.Label>¿El tutor proporciona retroalimentación constructiva y útil?</Form.Label>
-                <Form.Control as='textarea' rows={2} onChange={onChange} name='retroalimentacion' />
-              </Form.Group>
-            </Card.Body>
-          </Card>
-
-          {/* Satisfacción General */}
-          <Card className='mt-3'>
-            <Card.Body>
-              <h5>Satisfacción General</h5>
-              <Form.Group className='mb-3'>
-                <Form.Label>¿Qué es lo que más te gusta del tutor?</Form.Label>
-                <Form.Control as='textarea' rows={2} onChange={onChange} name='Likes' />
-              </Form.Group>
-              <Form.Group className='mb-3'>
-                <Form.Label>¿Hay algo que el tutor pueda mejorar?</Form.Label>
-                <Form.Control as='textarea' rows={2} onChange={onChange} name='mejorar' />
-              </Form.Group>
-              <Form.Group className='mb-3'>
-                <Form.Label>¿Recomendarías al tutor a otros estudiantes?</Form.Label>
-                <Form.Control as='textarea' rows={2} onChange={onChange} name='Recommendation' />
-              </Form.Group>
-            </Card.Body>
-          </Card>
-
-          <div className="mt-3">
-            <Button disabled={isEnabled} variant="outline-success" type='submit'>Enviar</Button>
-            <Button variant="outline-danger" type='reset'>Cancelar</Button>
-          </div>
-        </Card.Body>
-      </Card>
-    </Container>
-  );
+              ))}
+            </Form.Group>
+          ))}
+          <Button type="submit" variant="primary">
+            Enviar
+          </Button>
+        </Form>
+      </Card.Body>
+    </Card>
+  </Container>
+);
 }
 
 export default App;
